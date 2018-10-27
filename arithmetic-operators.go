80721c3a2f1ccfc37b35e4e350d562c5167b3670
Aniket Kulkarni <aniketmilindkulkarni@gmail.com>
2018-10-27 13:02:43 -0700

    Adding Arithmetic Operators & Sample App

---
+++ arithmetic-operators.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	add := 1 + 2
+	println(add)
+
+	subtract := 4 - 2
+	println(subtract)
+
+	remainder := 5 % 2
+	println(remainder)
+
+	inc := 1
+	inc++
+	println(inc)
+}
