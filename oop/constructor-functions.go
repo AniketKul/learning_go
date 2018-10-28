427c39e9afbfc2f46a4dafe9944b6883528c16d0
Aniket Kulkarni <aniketmilindkulkarni@gmail.com>
2018-10-27 17:34:22 -0700

    Constructor Functions

--- oop/constructor-functions.go
+++ oop/constructor-functions.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	foo := initializeMyStruct()
+	foo.myMap["India"] = "Delhi"
+	foo.myMap["Korea"] = "Seoul"
+	foo.myMap["Japan"] = "Tokyo"
+
+	fmt.Println(foo)
+}
+
+type myStruct struct {
+	myMap map[string]string
+}
+
+func initializeMyStruct() *myStruct {
+	result := myStruct{}
+	result.myMap = map[string]string{}
+	return &result
 }
